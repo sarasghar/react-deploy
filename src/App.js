03af383097331d1aba6f9513597592c7586350a2
sarasghar <sarasghar@gmail.com>
2020-04-26 17:24:44 +0500

    first react app deployment test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>hello I am Sara. This is my first react app deploy</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
