47d4d4ceb9ff69ec71bc282d7d2638b594feaaa7
sarasghar <sarasghar@gmail.com>
2020-04-27 15:22:52 +0500

    Updates

---
+++ precache-manifest.352ba04343801503ca877fbe703ed263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05db4d0066c4c588f04b1749ee01fe35",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "88b0d52f06a438db3986",
+    "url": "/react-deploy/static/css/main.4b738920.chunk.css"
+  },
+  {
+    "revision": "3c96c51b10b46a4294d2",
+    "url": "/react-deploy/static/js/2.da60933e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.da60933e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88b0d52f06a438db3986",
+    "url": "/react-deploy/static/js/main.cd89b7d8.chunk.js"
+  },
+  {
+    "revision": "f28f71bb60b037848c43",
+    "url": "/react-deploy/static/js/runtime-main.21e4d19d.js"
+  }
+]);
